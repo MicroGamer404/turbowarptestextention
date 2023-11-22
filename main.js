f6fe1a640529d07b810c7259dec0bcac775f49d8
MicroGamer404 <151774408+MicroGamer404@users.noreply.github.com>
2023-11-22 11:29:55 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+class TestExtension {
+  getInfo() {
+    return {
+      id: 'helloworld',
+      name: 'It works!',
+      blocks: [
+        {
+          opcode: 'hello',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'Hello!'
+        }
+      ]
+    };
+  }
+
+  hello() {
+    return 'World!';
+  }
+}
+
+scratch.extension.register(new TestExtension());
